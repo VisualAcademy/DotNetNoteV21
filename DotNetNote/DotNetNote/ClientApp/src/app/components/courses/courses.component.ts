b2b23cd6a010f43d2e024c2d6f351253532f5577
RedPlus <redplus@redplus.net>
2019-03-02 23:36:17 +0900

    Angular 소스 복원 중...

---
+++ DotNetNote/DotNetNote/ClientApp/src/app/components/courses/courses.component.ts
@@ -0,0 +1,18 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+import { CoursesDataService } from './courses.dataservice';
+
+@Component({
+    selector: "courses",
+    templateUrl: './courses.component.html'
+})
+export class CoursesComponent implements OnInit {
+    gitHubApi: any = {}; 
+    constructor(private ds: CoursesDataService) { }
+    ngOnInit() {
+        this.getGitHubApi(); 
+    }
+    getGitHubApi() {
+        this.ds.getGitHubApi().subscribe((gitHubApi) => { this.gitHubApi = gitHubApi; }); 
+    }
+}
