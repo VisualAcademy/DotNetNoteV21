553a8298748e3a367b5a9aae6007b8fc7c0bac68
RedPlus <redplus@redplus.net>
2019-03-03 23:46:26 +0900

    Angular Samples

---
+++ DotNetNote/DotNetNote/ClientApp/src/app/components/CheckAge/CheckAge.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'checkage',
+    templateUrl: './CheckAge.html'
+})
+export class CheckAge {
+    
+}
---
+++ DotNetNote/DotNetNote/ClientApp/src/app/components/animation/animation.basic.component.ts
@@ -0,0 +1,22 @@
+﻿import * as c from '@angular/core';
+//import * as a from '@angular/animations'; 
+
+@c.Component({
+    selector: 'animation-basic',
+    template: `
+        <h1>애니메이션 기본</h1>
+        <div *ngIf="exp">
+            활성화됨 
+        </div>
+        <button (click)="toggle()">활성화</button>
+    `,
+    animations: [
+
+    ]
+})
+export class AnimationBasicComponent {
+    exp: boolean = true; 
+    toggle(): void {
+        this.exp = !this.exp;
+    }
+}
