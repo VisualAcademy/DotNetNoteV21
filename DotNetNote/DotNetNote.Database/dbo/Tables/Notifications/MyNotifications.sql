a02c15d7f8f0153c36b94f97aa5ddafcd29dd9c1
RedPlus <redplus@redplus.net>
2019-03-06 06:35:28 +0900

    알림 서비스 애플리케이션을 위한 MyNotifications.sql 테이블 생성

---
+++ DotNetNote/DotNetNote.Database/dbo/Tables/Notifications/MyNotifications.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[MyNotifications](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Timestamp] [datetimeoffset](7) NOT NULL,
+	[Message] [nvarchar](max) NULL,
+	[Type] [nvarchar](50) NULL,
+	[Url] [nvarchar](max) NULL,
+ CONSTRAINT [PK_MyNotifications] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[MyNotifications] ADD  CONSTRAINT [DF_MyNotifications_Timestamp]  DEFAULT (sysdatetimeoffset()) FOR [Timestamp]
+GO
