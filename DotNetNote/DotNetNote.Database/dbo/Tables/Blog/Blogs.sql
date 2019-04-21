224fa0ac59176c5b58622043190b9d43f4d7b1c9
RedPlus <redplus@redplus.net>
2019-04-22 02:41:19 +0900

    Azure Web App에 게시 완료

--- DotNetNote/DotNetNote.Database/dbo/Tables/Blog/Blogs.sql
+++ DotNetNote/DotNetNote.Database/dbo/Tables/Blog/Blogs.sql
@@ -2,7 +2,7 @@
 (
 	[BlogId] INT NOT NULL PRIMARY KEY Identity(1, 1),
 	Name NVarChar(50) Not Null,
-	Url NVarChar(255) Null,
+	[Url] NVarChar(255) Null,
 	Description NVarChar(Max) Null,
 	BloggerName NVarChar(50) Null,						-- 블로거 이름
 	DateCreated DateTime Default(GetDate())
