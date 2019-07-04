e7c4254ef4dcbb32343fc5936bbedc262e101777
RedPlus <redplus@redplus.net>
2019-07-05 01:19:04 +0900

    날짜 관련 컬럼명 고민

--- DotNetNote/DotNetNote.Models/Notifications/MyNotification.cs
+++ DotNetNote/DotNetNote.Models/Notifications/MyNotification.cs
@@ -8,7 +8,7 @@ namespace DotNetNote.Models.Notifications
     public class MyNotification
     {
         public int Id { get; set; }
-        public DateTimeOffset Timestamp { get; set; }
+        public DateTimeOffset Timestamp { get; set; } // Created, Modified, CreatedDate, DateCreated, ...
         public string Message { get; set; }
         public string Type { get; set; }
         public string Url { get; set; }
